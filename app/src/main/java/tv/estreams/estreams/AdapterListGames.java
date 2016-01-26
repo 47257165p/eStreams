090d68fa8af43c0de130a9d74cac487010576e6a
Alejandro Soriano <47257165p@iespoblenou.org>
2016-01-26 14:33:57 +0100

    Insertada imagen de aplicación así como eliminada la opción de mostrar canales en español dada la imposibilidad de parsear la web después de ejecución de scripts y la poca versatilidad de la API. -Alejandro Soriano

--- app/src/main/java/tv/estreams/estreams/AdapterListGames.java
+++ app/src/main/java/tv/estreams/estreams/AdapterListGames.java
@@ -28,50 +28,9 @@ public class AdapterListGames extends ArrayAdapter <Stream>{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-
-        switch (position)
-        {
-            case 0:
-            {
-            LayoutInflater inflater = LayoutInflater.from(getContext());
-            convertView = inflater.inflate(R.layout.listview_text_game_type, parent, false);
-                TextView tVList = (TextView) convertView.findViewById(R.id.tVList);
-                tVList.setText("CANALES EN ESPAÑOL");
-                break;
-            }
-            case 1:
-            case 2:
-            {
-                LayoutInflater inflater = LayoutInflater.from(getContext());
-                convertView = inflater.inflate(R.layout.listview_image_game_type, parent, false);
-                Stream stream = getItem(position-1);
-                posterPath = "http://static-cdn.jtvnw.net/previews-ttv/live_user_"+stream.getChannel().getName()+"-320x180.jpg";
-
-                ImageView iVList = (ImageView) convertView.findViewById(R.id.iVList);
-                TextView tVListImage1 = (TextView) convertView.findViewById(R.id.tVListImage1);
-                TextView tVListImage2 = (TextView) convertView.findViewById(R.id.tVListImage2);
-                TextView tVListImage3 = (TextView) convertView.findViewById(R.id.tVListImage3);
-
-                tVListImage1.setText(stream.getChannel().getName());
-                tVListImage2.setText("reproduciendo "+stream.getGame());
-                tVListImage3.setText(stream.getChannel().getViews()+" espectadores");
-                
-                Picasso.with(getContext()).load(posterPath).fit().into(iVList);
-                break;
-            }
-            case 3:
-            {
-                LayoutInflater inflater = LayoutInflater.from(getContext());
-                convertView = inflater.inflate(R.layout.listview_text_game_type, parent, false);
-                TextView tVList = (TextView) convertView.findViewById(R.id.tVList);
-                tVList.setText("TODOS LOS CANALES");
-                break;
-            }
-            default:
-            {
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.listview_image_game_type, parent, false);
-                Stream stream = getItem(position-2);
+                Stream stream = getItem(position);
                 posterPath = "http://static-cdn.jtvnw.net/previews-ttv/live_user_"+stream.getChannel().getName()+"-320x180.jpg";
                 ImageView iVList = (ImageView) convertView.findViewById(R.id.iVList);
                 TextView tVListImage1 = (TextView) convertView.findViewById(R.id.tVListImage1);
@@ -82,9 +41,7 @@ public class AdapterListGames extends ArrayAdapter <Stream>{
                 tVListImage2.setText("reproduciendo "+stream.getGame());
                 tVListImage3.setText(stream.getChannel().getViews()+" espectadores");
                 Picasso.with(getContext()).load(posterPath).fit().into(iVList);
-                break;
-            }
-        }
+
         return convertView;
     }
 }
